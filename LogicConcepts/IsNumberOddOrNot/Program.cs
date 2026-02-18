5cc22a58f82e70ba41bbe3ff4d6460726c90a6ba
DIEGO ALEJANDRO CARDENAS AGUDELO <diegocardenas1151747@correo.itm.edu.co>
2026-02-18 12:24:21 -0500

    Complete Exercise

--- LogicConcepts/IsNumberOddOrNot/Program.cs
+++ LogicConcepts/IsNumberOddOrNot/Program.cs
@@ -6,7 +6,7 @@ var options = new List<string> { "s", "n" };
 
 do
 {
-    var number = ConsoleExtension.GetInt("Ingrese numero entero difrente de 0");
+    var number = ConsoleExtension.GetInt("Ingrese número entero difrente de 0");
     if (number == 0)
     {
         continue;
---
+++ LogicConcepts/TheLargestNuumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+
+do
+{
+    Console.Write("Ingrese tres números diferentes:");
+    var a = ConsoleExtension.GetInt("Ingrese el primer número");
+    var b = ConsoleExtension.GetInt("Ingrese el segundo número");
+    var c = ConsoleExtension.GetInt("Ingrese el tercer número");
+
+    if (a > b && a > c) 
+    {
+        Console.WriteLine($"El número {a}, es mayor ");
+    } 
+    else if (b>a && b>c)
+    {
+        Console.WriteLine($"El número {b}, es mayor ");
+    }
+    else
+    {
+        Console.WriteLine($"El número {c}, es mayor ");
+    }
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
